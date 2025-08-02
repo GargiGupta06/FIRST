4d6db303d39f1dfa32c697526da4cb10c94c3542
Gargi Gupta <gargigupta0612@gmail.com>
2025-08-02 11:02:01 +0530

    To add two numbers in python

---
+++ first.py
@@ -0,0 +1,11 @@
+# add_two_numbers.py
+
+# Get input from user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Add the numbers
+sum = num1 + num2
+
+# Display the result
+print("The sum is:", sum)
